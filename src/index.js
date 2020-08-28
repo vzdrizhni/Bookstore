707c0c77b488a3f1760e00c23d6c52a19f0f20ca
vzdrizhni <vzdrizhni@gmail.com>
2020-08-28 14:06:56 +0500

    Create initial state

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+const initialState = {
+  books: [
+    {
+      id: Math.floor(Math.random() * 100),
+      title: 'The Shining',
+      category: 'Horror'
+    },
+    {
+      id: Math.floor(Math.random() * 100),
+      title: 'Call of Cthulhu',
+      category: 'Horror'
+    },
+    {
+      id: Math.floor(Math.random() * 100),
+      title: 'For Whom the Bell Tolls',
+      category: 'Action'
+    }
+  ]
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
