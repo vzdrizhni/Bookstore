a09cd62d149ea2d361dc761ebbc54631eabaec99
vzdrizhni <vzdrizhni@gmail.com>
2020-08-30 00:04:02 +0500

    Eslint fixes

--- src/containers/booklist.js
+++ src/containers/booklist.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
-import {removeBook} from '../actions';
+import { removeBook } from '../actions';
 
-const BookList = ({books, removeBook}) => {
-  const handleBookRemove = (book) => removeBook(book)
+const BookList = ({ books, removeBook }) => {
+  const handleBookRemove = book => removeBook(book);
   return (
     <table>
       <thead>
@@ -16,29 +16,32 @@ const BookList = ({books, removeBook}) => {
         </tr>
       </thead>
       <tbody>
-        {books.map(book => (<Book
-          id={book.id}
-          title={book.title}
-          category={book.category}
-          key={book.id}
-          book={book}
-          removeBook={handleBookRemove}/>))}
+        {books.map(book => (
+          <Book
+            id={book.id}
+            title={book.title}
+            category={book.category}
+            key={book.id}
+            book={book}
+            removeBook={handleBookRemove}
+          />
+        ))}
       </tbody>
     </table>
-  )
+  );
 };
 
-const mapStateToProps = state => ({books: state});
+const mapStateToProps = state => ({ books: state });
 
 const mapDispatchToProps = dispatch => ({
-  removeBook: book => dispatch(removeBook(book))
+  removeBook: book => dispatch(removeBook(book)),
 });
 
 BookList.propTypes = {
   books: PropTypes
     .instanceOf(Object)
     .isRequired,
-  removeBook: PropTypes.func.isRequired
+  removeBook: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
