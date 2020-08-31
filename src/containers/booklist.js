9c08568685629fd2ca0a7d9fac376564dc0391eb
vzdrizhni <vzdrizhni@gmail.com>
2020-08-31 14:58:33 +0500

    Fix issues with displaying

--- src/containers/booklist.js
+++ src/containers/booklist.js
@@ -32,7 +32,7 @@ const BookList = ({ books, removeBook }) => {
   );
 };
 
-const mapStateToProps = state => ({ books: state });
+const mapStateToProps = state => ({ books: state.books });
 
 const mapDispatchToProps = dispatch => ({
   removeBook: book => dispatch(removeBook(book)),
