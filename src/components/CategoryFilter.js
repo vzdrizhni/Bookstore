90d08fae26db1883502e31401da2efd97bc64166
vzdrizhni <vzdrizhni@gmail.com>
2020-08-31 18:20:35 +0500

    Style first section

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CategoryFilter = ({ filterHandler }) => {
+const CategoryFilter = ({filterHandler}) => {
   const categories = [
     'Action',
     'Biography',
@@ -9,25 +9,37 @@ const CategoryFilter = ({ filterHandler }) => {
     'Horror',
     'Kids',
     'Learning',
-    'Sci-Fi',
+    'Sci-Fi'
   ];
 
   return (
-    <select onChange={filterHandler}>
-      <option>
-        All
-      </option>
-      {categories.map(category => (
-        <option value={category} key={category}>
-          {category}
-        </option>
-      ))}
-    </select>
+    <div className='panel-bg'>
+      <div className='booker'>
+        <div className='Bookstore-CMS'><h4>Bookstore CMS</h4></div>
+        <div className='BOOKS'>BOOKS</div>
+        <select className='CATEGORIES' onChange={filterHandler}>
+          <option>
+            CATEGORIES
+          </option>
+          {categories.map(category => (
+            <option value={category} key={category}>
+              {category}
+            </option>
+          ))}
+          <option>
+            All
+          </option>
+        </select>
+      </div>
+      <div className='Oval'>
+        <i className="fas fa-user-alt Mask"></i>
+      </div>
+    </div>
   );
 };
 
 CategoryFilter.propTypes = {
-  filterHandler: PropTypes.func.isRequired,
+  filterHandler: PropTypes.func.isRequired
 };
 
 export default CategoryFilter;
